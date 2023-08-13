e8cb2eb62a812a60020145f94ac50a9f72836080
Rômulo Pastore da Cruz Silva <romumu@pop-os.localdomain>
2023-08-13 17:10:22 -0300

    Criando configuracao inicial do terraform / ec2

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile = "romumu"
+  region  = "us-west-2"
+  shared_credentials_files  = ["/home/romumu/aws/credentials"]
+  
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-03f65b8614a860c29"
+  instance_type = "t2.micro"
+  key_name = "iac-alura"
+
+  tags = {
+    Name = "Maquina do Romumu"
+  }
+}
